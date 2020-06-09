f59417bfce71ccc15e017988e5985e1c2cf2d846
Fangzheng Liu <fzliu@mit.edu>
2020-06-09 09:21:38 -0400

    update_by_fangzhengliu

--- Code/Python_client/client.py
+++ Code/Python_client/client.py
@@ -3,25 +3,38 @@ import struct
 import time
 from bluepy.btle import UUID, Peripheral, DefaultDelegate
 
+from pylive import live_plotter
+import numpy as np
+
 red_uuid = UUID(0xddd1)
 
+size = 500
+x_vec = np.linspace(0,1,size+1)[0:-1]
+sensor_data = np.zeros(len(x_vec))
+line1 = []
+
 class MyDelegate(DefaultDelegate):
-    def __init__(self):
+    def __init__(self, sensor_data, line1):
         DefaultDelegate.__init__(self)
         # ... initialise here
+        self.sensor_data = sensor_data
+        self.line1 = line1
 
     def handleNotification(self, cHandle, data):
         val = binascii.b2a_hex(data)
         val = binascii.unhexlify(val)
         val = struct.unpack('f', val)[0]
-        print(str(val) + " deg C")
+        # print(str(val))
+        self.sensor_data[-1] = val
+        self.line1 = live_plotter(x_vec,self.sensor_data,self.line1)
+        self.sensor_data = np.append(self.sensor_data[1:],0.0)
 
 # Initialisation  -------
 
 p = Peripheral("D6:30:BD:2C:E4:58", "random")
 
 try:
-    p.setDelegate(MyDelegate())
+    p.setDelegate(MyDelegate(sensor_data, line1))
 
     # enable notification
     setup_data = b"\x01\x00"
